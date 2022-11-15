cdf7bea16438e1f75934f16a494b2cb299130b68
M-SK <legendary512@gmail.com>
2022-11-16 00:42:30 +0300

    .

--- Titanic - Machine Learning from Disaster/Titanic - ML_from_disaster.py
+++ Titanic - Machine Learning from Disaster/Titanic - ML_from_disaster.py
@@ -1,3 +1,10 @@
+"""
+This is a python application to solve this competition problem https://www.kaggle.com/competitions/titanic/overview
+Titanic - Machine Learning from Disaster
+"""
+# Import dependencies
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+
+df = pd.read_csv("dataset/train.csv")
